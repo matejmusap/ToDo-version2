c681163b55a339e94c9098f6cf8b19a288830480
matejmusap <matej.musap@gmail.com>
2018-09-16 19:45:14 +0200

    Second atempt

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var express = require("express"),
-    app     = express(),
-    port    = process.env.PORT || 3000,
-bodyParser  = require("body-parser");
-
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
+    
 var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/views'));
 
-app.use("/", function(req, res) {
+app.get('/', function(req, res){
     res.sendFile("index.html");
-})
+});
 
-app.use("/api/todos", todoRoutes);
+app.use('/api/todos', todoRoutes);
 
-app.listen(port, function(req, res) {
-    console.log("Server has started at " + port);
+app.listen(port, function(){
+    console.log("APP IS RUNNING ON PORT " + port);
 })
